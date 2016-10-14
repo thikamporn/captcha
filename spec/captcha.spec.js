5fad2d01d88a815df466968cb5b90331cc821545
Chalita-Kcs <chalitaseangproh@gmail.com>
2016-10-14 22:20:41 +0700

    Test case pattern 1 ( + or - )

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -30,7 +30,7 @@ function Left(P,L)
       		}
     		else
       		{
-      			return number[L].toString();
+      			return number[L];
       		}
 	}
 function Right(P,R)
@@ -41,6 +41,66 @@ function Right(P,R)
         }
       else
         {
-          return number[R].toString();
+          return number[R];
         }
     }
+describe('Captcha App' ,  function()
+{
+	describe('Pattern is 1', function()
+	{
+		const pattern = 1
+    ////////+
+    it('should return "1 + ZERO" when input is 1,1,1,0' , function()
+    {
+       let app = new Captcha(pattern,1,1,0);
+        expect(app.generate()).toEqual('1 + ZERO');
+    });
+    it('should return "1 + ONE" when input is 1,1,1,1' , function()
+    {
+      let app = new Captcha(pattern,1,1,1);
+       expect(app.generate()).toEqual('1 + ONE');
+    });
+    it('should return "1 + TWO" when input is 1,1,2,1' , function()
+    {
+      let app = new Captcha(pattern,1,1,2);
+       expect(app.generate()).toEqual('1 + TWO');
+    });
+    it('should return "1 + THREE" when input is 1,1,2,0' , function()
+    {
+       let app = new Captcha(pattern,1,1,3);
+        expect(app.generate()).toEqual('1 + THREE');
+    });
+    it('should return "1 + FOUR" when input is 1,1,1,4' , function()
+    {
+       let app = new Captcha(pattern,1,1,4);
+        expect(app.generate()).toEqual('1 + FOUR');
+    });
+    //////////-
+    it('should return "1 - FIVE" when input is 1,2,1,5' , function()
+    {
+       let app = new Captcha(pattern,2,1,5);
+        expect(app.generate()).toEqual('1 - FIVE');
+    });
+    it('should return "1 - SIX" when input is 1,2,1,6' , function()
+    {
+       let app = new Captcha(pattern,2,1,6);
+        expect(app.generate()).toEqual('1 - SIX');
+    });
+    it('should return "1 - SEVEN" when input is 1,2,1,7' , function()
+    {
+       let app = new Captcha(pattern,2,1,7);
+        expect(app.generate()).toEqual('1 - SEVEN');
+    });
+    it('should return "1 - EIGHT" when input is 1,2,1,8' , function()
+    {
+       let app = new Captcha(pattern,2,1,8);
+        expect(app.generate()).toEqual('1 - EIGHT');
+    });
+    it('should return "1 - NINE" when input is 1,2,1,9' , function()
+    {
+       let app = new Captcha(pattern,2,1,9);
+        expect(app.generate()).toEqual('1 - NINE');
+    });
+	});
+
+});
