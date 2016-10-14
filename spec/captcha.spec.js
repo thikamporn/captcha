ed979918966d779d222cf62e8ccca2b78b6c9648
Chalita-Kcs <chalitaseangproh@gmail.com>
2016-10-14 21:23:05 +0700

    Create Class Captcha

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,4 +1,11 @@
-describe('Captcha App' ,  function()
+const number = ['ZERO','ONE','TWO','THREE','FOUR','FIVE','SIX','SEVEN','EIGTH','NINE'];
+function Captcha(P,O,L,R)
 {
-  ////////////////
-})
+  this.generate = function()
+  {
+    let leftoperand = Left(P,L);
+    let rightoperand = Right(P,R);
+    let opera = Operator(O);
+    return  leftoperand +' '+ opera +' '+ rightoperand;
+  }
+}
